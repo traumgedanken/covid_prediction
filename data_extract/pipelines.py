efba4b9d6e0fbabf5a23182257ee822a543f2b37
Ihor Bulaievskyi <latente.traumgedanken@gmail.com>
2020-05-18 20:03:33 +0300

    Implement crawling of cases for specific country

--- data_extract/pipelines.py
+++ data_extract/pipelines.py
@@ -1,3 +1,4 @@
+from data_extract.spiders.wiki_spider import WikiSpider
 from data_extract.spiders.worldometer_spider import WorldometerSpider
 from storage import get_db_instance
 
@@ -10,5 +11,9 @@ class MongoPipeline:
             collection = self.db.get_collection('countries')
             if collection.count({'name': item['name']}) == 0:
                 collection.insert_one(item)
+
+        elif isinstance(spider, WikiSpider):
+            collection = self.db.find('cases')
+            collection.insert_many(item['cases'])
 
         return item
---
+++ data_extract/spiders/wiki_spider.py
@@ -0,0 +1,43 @@
+from scrapy import Spider, Request
+
+from storage import get_db_instance
+
+
+class WikiSpider(Spider):
+    name = 'wiki'
+    countries = get_db_instance().get_collection('countries')
+
+    def start_requests(self):
+        for country in self.countries.find():
+            yield Request(f'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_{country["name"]}',
+                          callback=self.parse)
+
+    def parse(self, response):
+        country = parse_country(response.xpath('//h1/text()').get())
+        rows = response.xpath('//b[text()="# of cases"]/../../../following-sibling::tr')
+        cases = []
+        for row in rows:
+            date_ = row.xpath('./td[1]/text()').get()
+            deaths = row.xpath('.//div[contains(@style, "#A50026")]/@title').get()
+            recoveries = row.xpath('.//div[contains(@style, "SkyBlue")]/@title').get()
+            active_cases = row.xpath('.//div[contains(@style, "Tomato")]/@title').get()
+            if date_ and date_.count('-') == 2:
+                cases.append({
+                    'country': country,
+                    'date': date_,
+                    'deaths': parse_number(deaths),
+                    'recoveries': parse_number(recoveries),
+                    'active_cases': parse_number(active_cases)
+                })
+        if cases:
+            yield {'cases': cases}
+
+
+def parse_number(number):
+    if not number:
+        return 0
+    return int(number)
+
+
+def parse_country(country):
+    return country.replace('COVID-19 pandemic in', '').replace('the', '').strip()
